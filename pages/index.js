771d7898d71b7cf5359fe3ab2f537e9a56811087
Steven Borblob <stevenjborik@protonmail.com>
2022-02-23 21:48:34 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,90 @@
+import NextLink from 'next/link' 
+import { Container, Box, Button, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import Section from '../components/section';
+import Paragraph from '../components/paragraph';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { BioSection, BioYear} from '../components/bio'
+import { Layout } from '../components/layouts/article';
+const Page = () => {
+    return (
+
+            <Container>
+                    <Box borderRadius="lg" bg="green.500" p={2.5} mb={6} align="center">
+                        A professional both in and out of the armchair.  
+                    </Box>
+                    <Box display = {{ md: 'flex' }}>
+                        <Box flexGrow={1}>
+                            <Heading as="h2" variant="page-title">
+                                Steven Borik
+                            </Heading>
+                            <p>Developer | Multi-Instrumentalist | Retired Semi-Pro Athlete</p>
+                        </Box>
+                        <Box flexShrink ={0} 
+                        mt={{ base:4, md:0 }}
+                        ml ={{ md:6 }} 
+                        align="center">
+                            <Image borderColor="whiteAplpha.800"
+                            borderWidth={2} 
+                            borderStyle="solid"
+                            maxWidth="100px"
+                            display="inline-block"
+                            borderRadius="full"
+                            src="/images/stevenp.jpg"
+                            alt="Profile Image" />
+                        </Box>
+                    </Box>
+                <Section delay={0.1}>
+                    <Heading as ="h3"variant="section-title">
+                        Me
+                    </Heading>
+                    <Paragraph>
+                        I'm a full stack software developer with 5 years of industry experience. I started my IT 
+                        career 7 years ago with Linux System Administration, and entered the dev world at JCPenney.
+                        I gained full stack experience through designing MVC and React Progressive Web Applications, and
+                        DevOps and CI/CD experience in monthly deployments of the JCPenney's Point of Sale Application. 
+                        
+                    </Paragraph>
+                    <Box align="center" my="4">
+                        {/* <NextLink href="works">
+                            <Button rightIcon={<ChevronRightIcon />} bg="green.500"> 
+                                Projects 
+                            </Button>
+                        </NextLink> */}
+                    </Box>
+                </Section>
+
+                <Section delay={0.2}>
+                    <Heading as="h3" variant="section-title">
+                        Timeline
+                    </Heading>
+                    <BioSection>
+                        <BioYear>1995</BioYear>  
+                        Born in Detroit, Michigan  
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2003</BioYear>  
+                        Moved to Phoenix, Arizona 
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2006</BioYear>  
+                        Settled in Dallas, Texas  
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2017</BioYear>  
+                        Graduated from the University of Texas at Dallas - B.S. Management Information Systems    
+                    </BioSection>
+                </Section>
+
+                <Section delay={0.3}>
+                    <Heading as="h3" variant="section-title">
+                        Free Time Fun
+                    </Heading>
+                    <Paragraph>
+                        Music, Ultimate Frisbee, Reading, German Sheperd, Fighting Games
+                    </Paragraph>
+                </Section>
+            </Container>
+       
+    )
+}
+export default Page
